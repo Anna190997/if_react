dabfdd0c56c53202c7320ac9d50e19876a33d64b
AnnaVerbilo <ann-verbilo@yandex.ru>
2022-09-26 18:33:46 +0300

    fixed footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,5 +1,6 @@
 import './footer.css';
 import FooterLinks from '../FooterLinks/footerLinks';
+import { about, propertyTypes, support } from '../../constats';
 
 const Footer = () => {
   return (
@@ -10,28 +11,25 @@ const Footer = () => {
             <div className="footer_items col-6">
               <ul className="about">
                 <li className="about_items">About</li>
-                <FooterLinks linksName="How Triphouse works" />
-                <FooterLinks linksName="Careers" />
-                <FooterLinks linksName="Privacy" />
-                <FooterLinks linksName="Terms" />
+                {about.map((item) => (
+                  <FooterLinks linksName={item} />
+                ))}
               </ul>
             </div>
             <div className="footer_items col-6">
               <ul className="property_types">
                 <li className="property_types_items">Property types</li>
-                <FooterLinks linksName="Guest houses" />
-                <FooterLinks linksName="Hotels" />
-                <FooterLinks linksName="Apartments" />
-                <FooterLinks linksName="Villas" />
-                <FooterLinks linksName="Holiday homes" />
-                <FooterLinks linksName="Hostels" />
+                {propertyTypes.map((item) => (
+                  <FooterLinks linksName={item} />
+                ))}
               </ul>
             </div>
             <div className="footer_items col-6">
               <ul className="support">
                 <li className="support_items">Support</li>
-                <FooterLinks linksName="Contact Customer Service" />
-                <FooterLinks linksName="FAQ" />
+                {support.map((item) => (
+                  <FooterLinks linksName={item} />
+                ))}
               </ul>
             </div>
           </div>
---
+++ src/constats.js
@@ -0,0 +1,14 @@
+const about = ['How Triphouse works', 'Careers', 'Privacy', 'Terms'];
+
+const propertyTypes = [
+  'Guest houses',
+  'Hotels',
+  'Apartments',
+  'Villas',
+  'Holiday homes',
+  'Hostels',
+];
+
+const support = ['Contact Customer Service', 'FAQ'];
+
+export { about, propertyTypes, support };
